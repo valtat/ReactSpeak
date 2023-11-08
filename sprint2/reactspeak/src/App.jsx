28a9095e2335bd8950c222acf57e29201a6f8990
Jonne Karhunen <jonne.karhunen@metropolia.fi>
2023-11-08 10:42:53 +0200

    Initialized project using Vite

---
+++ sprint2/reactspeak/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Welcome from "./components/Welcome";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Welcome />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ sprint2/reactspeak/src/components/Welcome.jsx
@@ -0,0 +1,9 @@
+const Welcome = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
+
+export default Welcome;
