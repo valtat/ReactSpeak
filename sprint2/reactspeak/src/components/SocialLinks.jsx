f2022d2c27b4de60522e11f14974e72a4f40e4a7
elenabli <elena.blinova@metropolia.fi>
2023-11-13 20:26:16 +0200

    remove tiktok-icon from SocialLinks.jxs

--- sprint2/reactspeak/src/components/SocialLinks.jsx
+++ sprint2/reactspeak/src/components/SocialLinks.jsx
@@ -3,11 +3,10 @@ import SocialLink from "./SocialLink";
 const SocialLinks = ({parentClass, itemClass}) => {
   return (
     <ul className={parentClass}>
-        <SocialLink link={{ id: 0, href: 'http://tiktok.com', icon: 'fab fa-tiktok' }} itemClass={itemClass} />
-        <SocialLink link={{ id: 1, href: 'http://facebook.com', icon: 'fab fa-facebook-f' }} itemClass={itemClass} />
-        <SocialLink link={{ id: 2, href: 'http://twitter.com', icon: 'fab fa-twitter' }} itemClass={itemClass} />
-        <SocialLink link={{ id: 3, href: 'http://instagram.com', icon: 'fab fa-instagram' }} itemClass={itemClass} />
-        <SocialLink link={{ id: 4, href: 'http://youtube.com', icon: 'fab fa-youtube' }} itemClass={itemClass} />
+        <SocialLink link={{id: 1, href: 'https://www.facebook.com', icon: 'fab fa-facebook-f'}} itemClass={itemClass}/>
+        <SocialLink link={{id: 2, href: 'https://www.twitter.com', icon: 'fab fa-twitter'}} itemClass={itemClass}/>
+        <SocialLink link={{id: 3, href: 'https://www.instagram.com', icon: 'fab fa-instagram'}} itemClass={itemClass}/>
+        <SocialLink link={{id: 4, href: 'https://www.youtube.com', icon: 'fab fa-youtube'}} itemClass={itemClass}/>
     </ul>
   );
 };
