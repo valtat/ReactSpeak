e7fa9599569cd6fd062c50655ac9a0728e1098f8
Jonne Karhunen <jonne.karhunen@metropolia.fi>
2023-11-20 14:22:42 +0200

    Backend port is now loaded from .env

--- sprint2/frontend/src/services/languageService.js
+++ sprint2/frontend/src/services/languageService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3000/api/v1/languages";
+const baseUrl = "/api/v1/languages";
 
 const getLanguage = async (language) => {
   const response = await axios.get(`${baseUrl}/${language}`);
