2ca1272b768c78d01f05cb92eb2769bca9a179f8
Jonne Karhunen <jonne.karhunen@metropolia.fi>
2023-11-21 21:23:31 +0200

    Small fixes, added basic MongoDB schema for language data, added route to add language data

--- sprint2/backend/controllers/languageController.js
+++ sprint2/backend/controllers/languageController.js
@@ -1,21 +1,45 @@
 const languageData = require("../models/dummyLanguageModel");
+const Language = require("../models/languageSchema");
 
-const getLanguage = (req, res) => {
-  console.log(req.params.language);
-  const languages = languageData.getLanguageData(req.params.language);
-  if (languages) {
-    res.status(200).json({
-      status: "success",
-      data: {
-        languages,
-      },
-    });
-  } else {
-    res.status(404).json({
-      status: "fail",
-      message: "Language not found",
-    });
+// Uses DUMMY DATA, refactor.
+const getLanguage = async (req, res) => {
+  try {
+    console.log(req.params.language);
+    const languages = await languageData.getLanguageData(req.params.language);
+    if (languages) {
+      res.status(200).json({
+        status: "success",
+        data: {
+          languages,
+        },
+      });
+    } else {
+      res.status(404).json({
+        status: "fail",
+        message: "Language not found",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 };
 
-module.exports = { getLanguage };
+const addNewPhrase = async (req, res) => {
+  const { language, phrase, translation } = req.body;
+  try {
+    let languageDoc = await Language.findOne({ language });
+
+    if (!languageDoc) {
+      languageDoc = new Language({ language });
+    }
+
+    languageDoc.phrases.push({ phrase, translation });
+
+    const savedLanguage = await languageDoc.save();
+    res.json(savedLanguage);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+module.exports = { getLanguage, addNewPhrase };
