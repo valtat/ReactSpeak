b7ab3a3c2ef1ea2ffd1817a9badc9e89e476ce83
Jonne Karhunen <jonne.karhunen@metropolia.fi>
2023-11-24 22:56:09 +0200

    Removed DS_Stores.

--- sprint3/backend/middleware/authMiddleware.js
+++ sprint3/backend/middleware/authMiddleware.js
@@ -65,46 +65,4 @@ const localAuth = (req, res, next) => {
   })(req, res, next);
 };
 
-const userAuthOld = (req, res, next) => {
-  const token = req.cookies.access_token;
-  if (token) {
-    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
-      if (err) {
-        return res.status(401).json({ message: "Not authorized" });
-      } else {
-        if (decodedToken.role !== "user" || decodedToken.role !== "admin") {
-          return res.status(401).json({ message: "Not authorized" });
-        } else {
-          next();
-        }
-      }
-    });
-  } else {
-    return res
-      .status(401)
-      .json({ message: "Not authorized, token not available" });
-  }
-};
-
-const adminAuthOld = (req, res, next) => {
-  const token = req.cookies.access_token;
-  if (token) {
-    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
-      if (err) {
-        return res.status(401).json({ message: "Not authorized" });
-      } else {
-        if (decodedToken.role !== "admin") {
-          return res.status(401).json({ message: "Not authorized" });
-        } else {
-          next();
-        }
-      }
-    });
-  } else {
-    return res
-      .status(401)
-      .json({ message: "Not authorized, token not available" });
-  }
-};
-
 module.exports = { userAuth, adminAuth, localAuth, jwtAuth, refreshAuth };
