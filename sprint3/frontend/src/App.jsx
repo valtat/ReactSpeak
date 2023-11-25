a1eaaad6d38cf00e0b6e388cd9c7057825cbfb61
Valerio Tatananni <valerio.tatananni@gmail.com>
2023-11-25 18:24:45 +0200

    Refactor Dashboard import path, delete unnecessary CSS import
    CSS import is now inside component and Dashboard component has been moved to components folder.

--- sprint3/frontend/src/App.jsx
+++ sprint3/frontend/src/App.jsx
@@ -15,16 +15,13 @@ import StudyView, {
 } from "./pages/StudyView/index.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
 import CountriesPage from "./pages/CountriesPage/index.jsx";
-import { Dashboard } from "./components/Dashboard.jsx";
+import { Dashboard } from "./components/Dashboard/Dashboard.jsx";
 import CountryPage from "./pages/CountryPage/index.jsx";
 import WelcomePage from "./pages/WelcomePage/index.jsx";
 import TestLayout from "./pages/TestLayout/index.jsx";
 
 // import "./assets/css/Country.css";
 import "./App.css";
-// import "./assets/css/Login.css";
-import "./assets/css/Dashboard.css";
-
 
 const router = createBrowserRouter(
   createRoutesFromElements(
