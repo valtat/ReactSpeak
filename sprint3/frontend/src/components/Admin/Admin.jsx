1cc71c5711473816b410280a8e66e5b249cadee6
KristiinaSa <kristiinainkeri@gmail.com>
2023-12-02 20:08:44 +0200

    admin page

---
+++ sprint3/frontend/src/components/Admin/Admin.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Admin.css";
+import AdminAddLanguage from "./AdminAddLanguage.jsx";
+
+const Admin = () => {
+  return (
+    <div className="parent-container">
+      <h1 className="title"> Admin page </h1>
+      <div className="admin-container">
+        <div className="options-container">
+          <h2 className="subtitle">Options</h2>
+          <div className="selection-box">
+            <button className="admin-button"> Add</button>
+            <button className="admin-button"> Edit </button>
+            <button className="admin-button"> Delete </button>
+          </div>
+        </div>
+        <div className="tools-container">
+          <AdminAddLanguage />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Admin;
