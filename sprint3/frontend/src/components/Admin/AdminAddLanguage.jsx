e964e37657199ac1117ff8c4246a964488e93517
KristiinaSa <kristiinainkeri@gmail.com>
2023-12-03 18:33:52 +0200

    some form editions

--- sprint3/frontend/src/components/Admin/AdminAddLanguage.jsx
+++ sprint3/frontend/src/components/Admin/AdminAddLanguage.jsx
@@ -6,10 +6,25 @@ const AdminAddLanguage = () => {
       <h2 className="subtitle">Add a new sentence</h2>
       <div className="tools">
         <form action="">
-          <label htmlFor="original-language">Original: </label>
-          <input type="text" name="original-language" id="original-language" />
-          <label htmlFor="translation">Translation: </label>
-          <input type="text" name="translation" id="translation" />
+          <div className="form-group">
+            <label htmlFor="language">Language: </label>
+            <select name="language" id="language">
+              <option value="English">English</option>
+              <option value="Spanish">Spanish</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="original-language">Original: </label>
+            <input
+              type="text"
+              name="original-language"
+              id="original-language"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="translation">Translation: </label>
+            <input type="text" name="translation" id="translation" />
+          </div>
         </form>
         <button className="admin-button">Save edits</button>
       </div>
