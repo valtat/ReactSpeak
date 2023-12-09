6af2d0d0141b23d8e5dd00980215502756211e74
Valerio Tatananni <valerio.tatananni@gmail.com>
2023-12-09 14:34:52 +0200

    Add user routes change password and delete account

--- sprint3/backend/routes/dummyUserRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers/userController");
-
-router.post("/forgot-password", userController.forgotPassword);
-
-router.post("/login", userController.login);
-
-module.exports = router;
