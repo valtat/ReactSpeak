6f86b0873be94b2514006e187cbfb4a20abd3d6c
Valerio Tatananni <valerio.tatananni@gmail.com>
2023-12-09 14:36:15 +0200

    Added time spent and latestresults to dashboard

--- sprint3/frontend/src/components/Dashboard/LatestResults.jsx
+++ sprint3/frontend/src/components/Dashboard/LatestResults.jsx
@@ -10,39 +10,37 @@ const quizResults = [
 
 export const LatestResults = () => {
   return (
-    <div className="parent-container">
-      <div className="latest-results-container">
-        <div className="column">
-          <div className="row">
-            <div className="small-container">
-              <h2>Latest Quiz Results</h2>
-            </div>
+    <div className="latest-results-container">
+      <div className="column">
+        <div className="row">
+          <div className="latest-results-small-container">
+            <h2>Latest Quiz Results</h2>
           </div>
-          {quizResults.map((quiz, index) => {
-            let variant;
-            if (quiz.result > 90) {
-              variant = "success";
-            } else if (quiz.result >= 30) {
-              variant = "primary";
-            } else {
-              variant = "danger";
-            }
-            return (
-              <div className="row" key={index}>
-                <div className="small-container">
-                  <h4>{quiz.language}</h4>
-                </div>
-                <div className="small-container">
-                  <ProgressBar
-                    now={quiz.result}
-                    label={`${quiz.result}%`}
-                    variant={variant}
-                  />
-                </div>
-              </div>
-            );
-          })}
         </div>
+        {quizResults.map((quiz, index) => {
+          let variant;
+          if (quiz.result > 90) {
+            variant = "success";
+          } else if (quiz.result >= 30) {
+            variant = "primary";
+          } else {
+            variant = "danger";
+          }
+          return (
+            <div className="row" key={index}>
+              <div className="latest-results-small-small-container">
+                <h4>{quiz.language}</h4>
+              </div>
+              <div className="latest-results-small-small-container">
+                <ProgressBar
+                  now={quiz.result}
+                  label={`${quiz.result}%`}
+                  variant={variant}
+                />
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
