b7333aa9189620411f676e28a75170d6d4e9116a
KristiinaSa <kristiinainkeri@gmail.com>
2023-12-10 13:02:49 +0200

    modular css and component shown based on chosen mode

--- sprint3/frontend/src/components/Admin/Admin.jsx
+++ sprint3/frontend/src/components/Admin/Admin.jsx
@@ -1,26 +1,51 @@
 import React from "react";
-import "./Admin.css";
+import { useState } from "react";
+import styles from "./Admin.module.css";
 import AdminAddLanguage from "./AdminAddLanguage.jsx";
 import AdminEditLanguage from "./AdminEditLanguage.jsx";
 import AdminDelete from "./AdminDelete.jsx";
 
 const Admin = () => {
+  const [activeMode, setActiveMode] = useState("add");
+
+  const toggleMode = (mode) => {
+    setActiveMode(mode);
+  };
+
   return (
-    <div className="parent-container">
-      <h1 className="title"> Admin page </h1>
-      <div className="admin-container">
-        <div className="options-container">
-          <h2 className="subtitle">Options</h2>
-          <div className="selection-box">
-            <button className="admin-button"> Add</button>
-            <button className="admin-button"> Edit </button>
-            <button className="admin-button"> Delete </button>
+    <div className={styles.parentContainer}>
+      <h1 className={styles.title}> Admin page </h1>
+      <div className={styles.adminContainer}>
+        <div className={styles.optionsContainer}>
+          <h2 className={styles.subtitle}>Options</h2>
+          <div className={styles.selectionBox}>
+            <button
+              className={styles.adminButton}
+              name="add"
+              onClick={() => toggleMode("add")}
+            >
+              Add
+            </button>
+            <button
+              className={styles.adminButton}
+              name="edit"
+              onClick={() => toggleMode("edit")}
+            >
+              Edit
+            </button>
+            <button
+              className={styles.adminButton}
+              name="delete"
+              onClick={() => toggleMode("delete")}
+            >
+              Delete
+            </button>
           </div>
         </div>
-        <div className="tools-container">
-          <AdminAddLanguage />
-          <AdminEditLanguage />
-          <AdminDelete />
+        <div className={styles.toolsContainer}>
+          {activeMode === "add" && <AdminAddLanguage />}
+          {activeMode === "edit" && <AdminEditLanguage />}
+          {activeMode === "delete" && <AdminDelete />}
         </div>
       </div>
     </div>
