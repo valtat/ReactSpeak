67c812cc38938fff1a45b8043322c0afbdd621fa
KristiinaSa <kristiinainkeri@gmail.com>
2023-12-10 13:06:26 +0200

    modular css, basic button functionality

--- sprint3/frontend/src/components/Admin/AdminDelete.jsx
+++ sprint3/frontend/src/components/Admin/AdminDelete.jsx
@@ -1,18 +1,44 @@
 import React from "react";
+import styles from "./Admin.module.css";
 
 const AdminDelete = () => {
+
+  const handleDelete = async (e) => {
+    e.preventDefault();
+    console.log("Delete");
+
+    // const response = await fetch(apiUrl, {
+    //   method: "DELETE",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(sentence),
+    // });
+    
+  };
+
   return (
     <div>
-      <h2 className="subtitle">Delete</h2>
-      <div className="tools">
-        <div className="form-group">
-          <label htmlFor="language">Options: </label>
-          <select name="language" id="language">
-            <option value="English">User</option>
-            <option value="Spanish">Sentence</option>
-          </select>
-        </div>
-        <button className="admin-button">Save edits</button>
+      <h2 className={styles.subtitle}>Delete</h2>
+      <div className={styles.tools}>
+        <form action="">
+          <div className="formGroup">
+            <label htmlFor="language">Language: </label>
+            <select name="language" id="language">
+              <option value="English">English</option>
+              <option value="Spanish">Spanish</option>
+            </select>
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="original-language">Phrase in English: </label>
+            <input
+              type="text"
+              name="original-language"
+              id="original-language"
+            />
+          </div>
+        </form>
+        <button className={styles.adminButton} onClick={handleDelete}>Delete</button>
       </div>
     </div>
   );
