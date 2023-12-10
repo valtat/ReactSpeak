fc440226c19be05a437fd27e4bc4ac663f4fefe2
KristiinaSa <kristiinainkeri@gmail.com>
2023-12-10 13:05:10 +0200

    modular css, button functionality and added text for current translation

--- sprint3/frontend/src/components/Admin/AdminEditLanguage.jsx
+++ sprint3/frontend/src/components/Admin/AdminEditLanguage.jsx
@@ -1,32 +1,82 @@
 import React from "react";
+import styles from "./Admin.module.css";
+import { useState } from "react";
 
 const AdminEditLanguage = () => {
+  const [sentence, setSentence] = useState({
+    language: "English",
+    original: "",
+    translation: "",
+  });
+
+  const handleChange = (e) => {
+    setSentence({
+      ...sentence,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleEdit = async (e) => {
+    e.preventDefault();
+    console.log("Edit");
+    console.log(sentence);
+
+    // const response = await fetch(apiUrl, {
+    //   method: "PUT",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(sentence),
+    // });
+  };
+
   return (
     <div>
-      <h2 className="subtitle">Edit a sentence</h2>
-      <div className="tools">
+      <h2 className={styles.subtitle}>Edit a sentence</h2>
+      <div className={styles.tools}>
         <form action="">
-          <div className="form-group">
+          <div className={styles.formGroup}>
             <label htmlFor="language">Language: </label>
-            <select name="language" id="language">
+            <select
+              name="language"
+              id="language"
+              value={sentence.language}
+              onChange={handleChange}
+            >
               <option value="English">English</option>
               <option value="Spanish">Spanish</option>
             </select>
           </div>
-          <div className="form-group">
-            <label htmlFor="original-language">Original: </label>
+          <div className={styles.formGroup}>
+            <label htmlFor="original-language">Phrase in English: </label>
             <input
               type="text"
-              name="original-language"
-              id="original-language"
+              name="original"
+              id="original"
+              value={sentence.original}
+              onChange={handleChange}
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="translation">Translation: </label>
-            <input type="text" name="translation" id="translation" />
+          <div className={styles.formGroup}>
+            <label htmlFor="current-translation">Current translation: </label>
+            <div name="current-translation" id="current-translation">
+              This is the current translation
+            </div>
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="translation">New translation: </label>
+            <input
+              type="text"
+              name="translation"
+              id="translation"
+              value={sentence.translation}
+              onChange={handleChange}
+            />
           </div>
         </form>
-        <button className="admin-button">Save edits</button>
+        <button className={styles.adminButton} onClick={handleEdit}>
+          Save edits
+        </button>
       </div>
     </div>
   );
