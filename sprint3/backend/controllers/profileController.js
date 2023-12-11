9128d97e7f0299d2d99d433565cdc12124228840
Valerio Tatananni <valerio.tatananni@gmail.com>
2023-12-11 20:17:28 +0200

    Refactor profileController.js
    Uses profileSchema and add updateDefaultLanguage function

--- sprint3/backend/controllers/profileController.js
+++ sprint3/backend/controllers/profileController.js
@@ -1,10 +1,30 @@
-const profileModel = require("../models/dummyProfileModel");
+const Profile = require("../models/profileSchema");
 
-const returnProfile = (req, res) => {
-  const profile = profileModel.returnProfile();
+const returnProfile = async (req, res) => {
+  const { userId } = req.params;
+  const profile = await Profile.findOne({ user: userId });
   res.status(200).json(profile);
+};
+
+const updateDefaultLanguage = async (req, res) => {
+  const { userId, language } = req.body;
+
+  try {
+    const profile = await Profile.findOne({ user: userId });
+    if (!profile) {
+      return res.status(404).json({ message: "Profile not found" });
+    }
+
+    profile.defaultLanguage = language;
+    await profile.save();
+
+    res.status(200).json({ message: "Default language updated successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
 module.exports = {
   returnProfile,
+  updateDefaultLanguage,
 };
