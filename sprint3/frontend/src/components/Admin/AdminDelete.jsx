0afa281ff22f8413888dd52c2230d762b6a18bbd
KristiinaSa <kristiinainkeri@gmail.com>
2023-12-11 20:49:05 +0200

    Added confirm deletion modal

--- sprint3/frontend/src/components/Admin/AdminDelete.jsx
+++ sprint3/frontend/src/components/Admin/AdminDelete.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import styles from "./Admin.module.css";
+import { useState } from "react";
 
 const AdminDelete = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleDelete = async (e) => {
     e.preventDefault();
@@ -14,15 +17,39 @@ const AdminDelete = () => {
     //   },
     //   body: JSON.stringify(sentence),
     // });
-    
+    closeModal();
   };
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  // const handleModal = () => {
+  //   const modal = document.querySelector(".modal");
+  //   modal.style.display = "block";
+
+  //   window.onclick = function (event) {
+  //     if (event.target === modal) {
+  //       modal.style.display = "none";
+  //     }
+  //   };
+
+  //   const span = document.querySelector(".close");
+  //   span.onclick = function () {
+  //     modal.style.display = "none";
+  //   };
+  // };
 
   return (
     <div>
       <h2 className={styles.subtitle}>Delete</h2>
       <div className={styles.tools}>
         <form action="">
-          <div className="formGroup">
+          <div className={styles.formGroup}>
             <label htmlFor="language">Language: </label>
             <select name="language" id="language">
               <option value="English">English</option>
@@ -38,7 +65,26 @@ const AdminDelete = () => {
             />
           </div>
         </form>
-        <button className={styles.adminButton} onClick={handleDelete}>Delete</button>
+        <button className={styles.adminButton} onClick={openModal}>
+          Delete
+        </button>
+
+        {isOpen && (
+          <div className={styles.modalContent}>
+          <p className={styles.subtitle}>
+            Are you sure you want to delete this phrase from the database?
+          </p>
+          <p className={styles.paragraph}> All translations will be removed.</p>
+          <div className={styles.modalsButtons}>
+            <button className={styles.adminButton} onClick={handleDelete}>
+              Yes
+            </button>
+            <button className={styles.adminButton} onClick={closeModal}>
+              No
+            </button>
+          </div>
+      </div>
+        )}
       </div>
     </div>
   );
