5d6f008645476dfb4c5724d622891cb194c3d513
Valerio Tatananni <valerio.tatananni@gmail.com>
2023-12-11 17:48:47 +0200

    Make modal and redirect to language selection

--- sprint3/frontend/src/components/Country/Language.jsx
+++ sprint3/frontend/src/components/Country/Language.jsx
@@ -1,19 +1,38 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useState, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+import Modal from "react-modal";
 import "./CountryInformation.css";
 
 function Language({ icon, languageName }) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const handleOpenModal = useCallback(() => {
+    setModalIsOpen(true);
+  }, []);
+
   return (
-      <div className="language-card">
-        <Link to={`/${languageName}`} className="language-link">
-          <div className="language-image-container">
-            <img src={icon} alt={languageName} />
-          </div>
-          <div className="language-footer">
-            <h3>{languageName}</h3>
-          </div>
-        </Link>
+    <div className="language-card">
+      <div onClick={handleOpenModal} className="language-link">
+        <div className="language-image-container">
+          <img src={icon} alt={languageName} />
+        </div>
+        <div className="language-footer">
+          <h3>{languageName}</h3>
+        </div>
       </div>
+      <Modal
+        className="modal"
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+      >
+        <h2>Start studying {languageName}?</h2>
+        <p>By clicking Start, you will be redirected to the study session.</p>
+        <button onClick={() => navigate("/study")}>Start</button>
+        <button onClick={() => setModalIsOpen(false)}>Close</button>
+      </Modal>
+    </div>
   );
 }
 
