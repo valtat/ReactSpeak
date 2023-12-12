a3d10f575929dd6b46742de31dabe9532f10b8f5
KristiinaSa <kristiinainkeri@gmail.com>
2023-12-12 19:41:33 +0200

    deleting a phrase works

--- sprint3/frontend/src/components/Admin/AdminDelete.jsx
+++ sprint3/frontend/src/components/Admin/AdminDelete.jsx
@@ -1,47 +1,54 @@
 import React from "react";
 import styles from "./Admin.module.css";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import adminService from "../../services/adminService";
 
 const AdminDelete = () => {
-
   const [isOpen, setIsOpen] = useState(false);
+  const [deleteSuccessfull, setDeleteSuccessfull] = useState(false);
+  const [deleteFailed, setDeleteFailed] = useState(false);
+
   const [translation, setTranslation] = useState({
     englishMeaning: "",
     language: "",
   });
 
   const handleChange = (e) => {
+    let value = e.target.value;
+    if (e.target.name === "language") {
+      value = value.toLowerCase();
+    }
     setTranslation({
       ...translation,
-      [e.target.name]: e.target.value,
+      [e.target.name]: value,
     });
   };
 
   const handleDelete = async (e) => {
     e.preventDefault();
     console.log("Delete");
+    console.log("Translation:", translation);
 
-    const apiUrl = "/api/v1/delete-translation";
-
-    try{
-      const response = await fetch(apiUrl, {
-        method : "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(translation),
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    console.log("Success:", data);
+    try {
+      const data = await adminService.deleteTranslation(translation);
+      console.log("Phrase deleted successfully:", data);
+      setDeleteSuccessfull(true);
+      setTimeout(() => {
+        setDeleteSuccessfull(false);
+        setTranslation({
+          englishMeaning: "",
+          language: "",
+        });
+      }, 2000);
     } catch (error) {
       console.error("An error occurred while deleting the phrase", error);
+      setDeleteFailed(true);
+      setTimeout(() => {
+        setDeleteFailed(false);
+      }, 2000);
     }
-
     closeModal();
   };
 
@@ -60,9 +67,10 @@ const AdminDelete = () => {
         <form action="">
           <div className={styles.formGroup}>
             <label htmlFor="language">Language: </label>
-            <select name="language" id="language" onChange = {handleChange}>
+            <select name="language" id="language" onChange={handleChange}>
               <option value="English">English</option>
               <option value="Spanish">Spanish</option>
+              <option value="French">French</option>
             </select>
           </div>
           <div className={styles.formGroup}>
@@ -71,7 +79,7 @@ const AdminDelete = () => {
               type="text"
               name="englishMeaning"
               id="englishMeaning"
-              onChange = {handleChange}
+              onChange={handleChange}
             />
           </div>
         </form>
@@ -81,19 +89,28 @@ const AdminDelete = () => {
 
         {isOpen && (
           <div className={styles.modalContent}>
-          <p className={styles.subtitle}>
-            Are you sure you want to delete this phrase from the database?
-          </p>
-          <p className={styles.paragraph}> All translations will be removed.</p>
-          <div className={styles.modalsButtons}>
-            <button className={styles.adminButton} onClick={handleDelete}>
-              Yes
-            </button>
-            <button className={styles.adminButton} onClick={closeModal}>
-              No
-            </button>
+            <p className={styles.subtitle}>
+              Are you sure you want to delete this phrase from the database?
+            </p>
+            <div className={styles.modalsButtons}>
+              <button className={styles.adminButton} onClick={handleDelete}>
+                Yes
+              </button>
+              <button className={styles.adminButton} onClick={closeModal}>
+                No
+              </button>
+            </div>
           </div>
-      </div>
+        )}
+
+        {deleteFailed && (
+          <p className={styles.paragraph}>Phrase not deleted. Try again.</p>
+        )}
+
+        {deleteSuccessfull && (
+          <p className={styles.paragraph}>
+            Phrase deleted! <FontAwesomeIcon icon={faCheck} />
+          </p>
         )}
       </div>
     </div>
