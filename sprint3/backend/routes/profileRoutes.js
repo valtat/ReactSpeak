615672b2fb98c11bd7ac38c7e4f911265ca68408
Valerio Tatananni <valerio.tatananni@gmail.com>
2023-12-12 18:33:57 +0200

    Update defaultLanguage route to use jwtAuth middleware

--- sprint3/backend/routes/profileRoutes.js
+++ sprint3/backend/routes/profileRoutes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { returnProfile } = require("../controllers/profileController");
 const { updateDefaultLanguage } = require("../controllers/profileController");
+const { jwtAuth } = require("../middleware/authMiddleware");
 
 router.get("/", returnProfile);
-router.put("/defaultLanguage", updateDefaultLanguage);
+router.post("/defaultLanguage", jwtAuth, updateDefaultLanguage);
 
 module.exports = router;
