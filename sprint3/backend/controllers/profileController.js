af9e67aaf413e2c0560a94d1b8990ebe606e8f4a
Valerio Tatananni <valerio.tatananni@gmail.com>
2023-12-12 18:34:35 +0200

    Refactor profileController.js to use req.user and correctly save default language to db

--- sprint3/backend/controllers/profileController.js
+++ sprint3/backend/controllers/profileController.js
@@ -1,16 +1,22 @@
 const Profile = require("../models/profileSchema");
+const mongoose = require("mongoose");
 
 const returnProfile = async (req, res) => {
-  const { userId } = req.params;
-  const profile = await Profile.findOne({ user: userId });
+  const user = req.user;
+  const profile = await Profile.findOne({
+    user: user._id,
+  });
   res.status(200).json(profile);
 };
 
 const updateDefaultLanguage = async (req, res) => {
-  const { userId, language } = req.body;
+  const user = req.user;
+  const { language } = req.body;
 
   try {
-    const profile = await Profile.findOne({ user: userId });
+    const profile = await Profile.findOne({
+      user: user._id,
+    });
     if (!profile) {
       return res.status(404).json({ message: "Profile not found" });
     }
@@ -20,6 +26,7 @@ const updateDefaultLanguage = async (req, res) => {
 
     res.status(200).json({ message: "Default language updated successfully" });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ message: "Server error" });
   }
 };
